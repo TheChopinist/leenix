5dada83cd7ab9156b34a2f352194bbf1955aa100
lee <dominik@axodo.ch>
2025-03-19 21:17:27 +0100

    Update NixOS configuration

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,10 +11,7 @@
 
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.leenux = nixpkgs.lib.nixosSystem {
-      specialArgs = {
-        inherit inputs;
-        hardwareConfig = /etc/nixos/hardware-configuration.nix;  # Absolute path
-      };
+      specialArgs = { inherit inputs; };  # Remove hardwareConfig
       modules = [
         ./configuration.nix
         inputs.home-manager.nixosModules.default
