98144970109321bee985cc3c04d5f740e8a4f247
Lee <domi@yoosana.com>
2025-04-04 22:16:57 +0200

    remove window shit who needs that shit anyways.

--- home/home.nix
+++ home/home.nix
@@ -43,6 +43,7 @@
       font_family = "Fira Code";
       background_opacity = "0.7";
       confirm_os_window_close = 0;
+      hide_window_decorations = true;
     };
   };
   programs.home-manager.enable = true;
