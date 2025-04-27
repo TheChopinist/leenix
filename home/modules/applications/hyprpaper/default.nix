1e4a1b8654328fb55929c9c2bebbc97c90bf345d
Lee <domi@yoosana.com>
2025-04-27 23:02:48 +0200

    g

--- home/modules/applications/hyprpaper/default.nix
+++ home/modules/applications/hyprpaper/default.nix
@@ -1,12 +1,18 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  # 1. First copy the wallpaper to a known location
+  wallpaper = pkgs.runCommand "wallpaper" {} ''
+    mkdir -p $out
+    cp ${./wallpaper.jpg} $out/wallpaper.jpg
+  '';
+in {
+  # 2. Then configure hyprpaper to use it
   services.hyprpaper = {
     enable = true;
     settings = {
       ipc = "on";
       splash = false;
-
-      preload = ./wallpapers/wallpaper.jpg;
-      wallpaper = ",./wallpapers/wallpaper.jpg";
+      preload = "${wallpaper}/wallpaper.jpg";
+      wallpaper = ",${wallpaper}/wallpaper.jpg";
     };
   };
 }
