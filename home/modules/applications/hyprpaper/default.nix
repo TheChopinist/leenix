f703079e54c4a3060d4ac2dce8dd27fe2edb51d3
Lee <domi@yoosana.com>
2025-04-27 23:00:27 +0200

    g

--- home/modules/applications/hyprpaper/default.nix
+++ home/modules/applications/hyprpaper/default.nix
@@ -1,14 +1,12 @@
 {pkgs, ...}: {
-  # Copy the wallpaper to a reliable location
-  home.file.".config/hypr/wallpaper.jpg".source = ./wallpapers/wallpaper.jpg;
-
   services.hyprpaper = {
     enable = true;
     settings = {
       ipc = "on";
       splash = false;
-      preload = "~/.config/hypr/wallpaper.jpg";
-      wallpaper = ",~/.config/hypr/wallpaper.jpg";
+
+      preload = ./wallpapers/wallpaper.jpg;
+      wallpaper = ",./wallpapers/wallpaper.jpg";
     };
   };
 }
