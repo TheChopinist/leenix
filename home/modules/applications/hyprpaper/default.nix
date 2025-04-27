e201066e63b04c8207c2cae4baae95e8d27b58aa
Lee <domi@yoosana.com>
2025-04-27 23:07:40 +0200

    s

--- home/modules/applications/hyprpaper/default.nix
+++ home/modules/applications/hyprpaper/default.nix
@@ -1,18 +1,55 @@
-{pkgs, ...}: let
-  # 1. First copy the wallpaper to a known location
-  wallpaper = pkgs.runCommand "wallpaper" {} ''
-    mkdir -p $out
-    cp ${./wallpaper.jpg} $out/wallpaper.jpg
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  # Get all wallpaper files from the wallpapers directory
+  wallpapers = lib.cleanSourceWith {
+    src = ./wallpapers;
+    filter = name: type:
+      type == "directory" || lib.strings.hasSuffix ".jpg" name || lib.strings.hasSuffix ".png" name;
+  };
+
+  # Script to randomly select and set a wallpaper
+  randomWallpaperScript = pkgs.writeShellScriptBin "hyprpaper-random" ''
+    WALLPAPERS=(${toString wallpapers}/*)
+    SELECTED_WALL=''${WALLPAPERS[$RANDOM % ''${#WALLPAPERS[@]}]}
+
+    hyprctl hyprpaper unload all
+    hyprctl hyprpaper preload "$SELECTED_WALL"
+    hyprctl hyprpaper wallpaper ",''${SELECTED_WALL}"
   '';
 in {
-  # 2. Then configure hyprpaper to use it
+  # Systemd service to run at startup and periodically
+  systemd.user.services.hyprpaper-random = {
+    Unit = {
+      Description = "Random Hyprpaper Wallpaper";
+      After = ["graphical-session.target" "hyprpaper.service"];
+      PartOf = ["graphical-session.target"];
+    };
+    Service = {
+      ExecStart = "${randomWallpaperScript}/bin/hyprpaper-random";
+      Restart = "on-failure";
+    };
+    Install.WantedBy = ["graphical-session.target"];
+  };
+
+  systemd.user.timers.hyprpaper-random = {
+    Unit.Description = "Timer for random Hyprpaper wallpapers";
+    Timer = {
+      OnBootSec = "10s";
+      OnUnitActiveSec = "1h";
+      Unit = "hyprpaper-random.service";
+    };
+    Install.WantedBy = ["timers.target"];
+  };
+
+  # Hyprpaper configuration
   services.hyprpaper = {
     enable = true;
     settings = {
       ipc = "on";
       splash = false;
-      preload = "${wallpaper}/wallpaper.jpg";
-      wallpaper = ",${wallpaper}/wallpaper.jpg";
     };
   };
 }
